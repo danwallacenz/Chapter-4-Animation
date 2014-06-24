513036c30c6e54d5b31fafe61b293153eca13c41
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-06-24 19:32:54 +1200

    UIImageView.animationImages

--- Chapter-4-Animations/Chapter-4-Animations/FirstViewController.m
+++ Chapter-4-Animations/Chapter-4-Animations/FirstViewController.m
@@ -9,36 +9,67 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet UIView *animationImagesView;
+@property (weak, nonatomic) IBOutlet UIStepper *durationStepper;
+@property (weak, nonatomic) IBOutlet UIStepper *repeatStepper;
+@property (weak, nonatomic) IBOutlet UILabel *durationLabel;
 
+@property (weak, nonatomic) IBOutlet UILabel *repeatLabel;
+@property (weak, nonatomic) IBOutlet UIButton *stopButton;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+@property UIImageView *flashingImageView;
 @end
 
 @implementation FirstViewController
 
+- (IBAction)durationStepperChanged:(UIStepper *)sender
+{
+    self.durationLabel.text = [NSString stringWithFormat:@"%0.f ms", sender.value ];
+}
+
+- (IBAction)repeatStepperChanged:(UIStepper *)sender
+{
+        self.repeatLabel.text = [NSString stringWithFormat:@"%0.f", sender.value ];
+    
+}
+
+- (IBAction)flashButtonPressed:(id)sender
+{
+    [self flashMars];
+}
+
+- (IBAction)stopButtonPressed:(UIButton *)sender
+{
+    [self.flashingImageView stopAnimating];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.repeatLabel.text = [NSString stringWithFormat:@"%0.f", self.repeatStepper.value ];
     
+    self.durationLabel.text = [NSString stringWithFormat:@"%0.f ms", self.durationStepper.value ];
+
+}
+
+-(void)flashMars
+{
     UIImage *mars = [UIImage imageNamed: @"Mars"];
     UIGraphicsBeginImageContextWithOptions(mars.size, NO, 0);
     UIImage *empty = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    NSArray *animationImages = @[mars, empty, mars, empty, mars];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage: empty];
-    CGRect imageViewFrame = imageView.frame;
-    imageViewFrame.origin = CGPointMake(100, 100);
-    imageView.frame =  imageViewFrame;
-    [self.view addSubview: imageView];
-    imageView.animationImages = animationImages;
-    imageView.animationDuration = 2;
-    imageView.animationRepeatCount = 1;
-    [imageView startAnimating];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSArray *animationImages = @[mars, empty];
+    self.flashingImageView = [[UIImageView alloc] initWithImage: empty];
+    CGRect imageViewFrame = self.flashingImageView.frame;
+    imageViewFrame.origin = CGPointMake(10, 10);
+    self.flashingImageView.frame =  imageViewFrame;
+    [self.animationImagesView addSubview: self.flashingImageView];
+    self.flashingImageView.animationImages = animationImages;
+    self.flashingImageView.animationDuration = self.durationStepper.value/ 1000.0;
+    self.flashingImageView.animationRepeatCount = self.repeatStepper.value;
+    [self.flashingImageView startAnimating];
 }
 
 @end
