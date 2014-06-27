229968e9bfdffe80f79f9aa97b1946d74a4753f1
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-06-27 22:05:49 +1200

    springing animation

---
+++ Chapter-4-Animations/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  Chapter-4-Animations
+//
+//  Created by Daniel Wallace on 27/06/14.
+//  Copyright (c) 2014 nz.co.datacom.danielw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
