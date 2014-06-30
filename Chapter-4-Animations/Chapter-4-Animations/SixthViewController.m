29b3f8e282d4aa3d8ba52fa129dc3f0a3068048d
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-06-30 14:38:14 +1200

    add waggle

--- Chapter-4-Animations/Chapter-4-Animations/SixthViewController.m
+++ Chapter-4-Animations/Chapter-4-Animations/SixthViewController.m
@@ -19,11 +19,36 @@
 
 @implementation SixthViewController
 
-#pragma mark - rotations
+#pragma mark - waggles
 
+- (IBAction)waggle01ButtonPressed
+{
+    // Capture the start and end values.
+    CATransform3D nowValue = self.arrow.transform;
+    CATransform3D startValue = CATransform3DRotate(nowValue, M_PI/40.0, 0, 0, 1);
+    CATransform3D endValue = CATransform3DRotate(nowValue, -M_PI/40.0, 0, 0, 1);
+    
+    // Construct the explicit animation.
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform"];
+    animation.duration = 0.05;
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    animation.repeatCount = 3;
+    animation.autoreverses = YES;
+    animation.fromValue = [NSValue valueWithCATransform3D:startValue];
+    animation.toValue = [NSValue valueWithCATransform3D:endValue];
+    
+    // Ask for the explicit animation.
+    [self.arrow addAnimation:animation forKey:nil];
+}
+
+#pragma mark - rotations
 
 - (IBAction)rotate01ConsensedButtonPressed
 {
+    /*
+     Once you know the full form, you will find that in many cases it can be condensed. For example, when fromValue and toValue are not set, the former and current values of the property are used automatically. (This magic is possible because the presentation layer still has the former value of the property, while the layer itself has the new value.) Thus, in this case there was no need to set them, and so there was no need to capture the start and end values beforehand either.
+     */
+    
     [CATransaction setDisableActions:YES];
     self.arrow.transform = CATransform3DRotate(self.arrow.transform, M_PI/4.0, 0, 0, 1);
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform"];
