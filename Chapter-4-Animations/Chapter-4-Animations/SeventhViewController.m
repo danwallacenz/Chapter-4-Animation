1fce1dcd3793ac017ffc4c24a70fcb302577a58d
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-07-05 15:46:24 +1200

    add a second firehose emitterlayer

--- Chapter-4-Animations/Chapter-4-Animations/SeventhViewController.m
+++ Chapter-4-Animations/Chapter-4-Animations/SeventhViewController.m
@@ -9,37 +9,35 @@
 #import "SeventhViewController.h"
 
 @interface SeventhViewController ()
-@property (strong, nonatomic) CAEmitterLayer *emitterLayer;
+@property (strong, nonatomic) CAEmitterLayer *emitterLayer1;
 
-@property (strong, nonatomic) CAEmitterCell *grayCircleImageCell;
+@property (strong, nonatomic) CAEmitterLayer *emitterLayer2;
 
-@property (weak, nonatomic) IBOutlet UISlider *birthRateSlider;
+//@property (strong, nonatomic) CAEmitterCell *grayCircleImageCell;
 
-@property (weak, nonatomic) IBOutlet UILabel *birthRateSliderValueLabel;
+//@property (weak, nonatomic) IBOutlet UISlider *birthRateSlider;
+//
+//@property (weak, nonatomic) IBOutlet UILabel *birthRateSliderValueLabel;
 @end
 
 @implementation SeventhViewController
 
 #pragma mark - IBActions
 
-- (IBAction)birthRateSliderValueChanged:(UISlider *)sender
-{
-    self.birthRateSliderValueLabel.text = [NSString stringWithFormat:@"%0.f", sender.value];
-    
-//    // Recreate emiiter layer.
-    [self.emitterLayer removeFromSuperlayer];
-    self.emitterLayer = nil;
-    
-    self.emitterLayer = [self emitterLayer];
-    
-    self.grayCircleImageCell.birthRate = floor(sender.value);
-    
-    [self.view.layer addSublayer: self.emitterLayer];
-    
-
-    
-    
-}
+//- (IBAction)birthRateSliderValueChanged:(UISlider *)sender
+//{
+//    self.birthRateSliderValueLabel.text = [NSString stringWithFormat:@"%0.f", sender.value];
+//    
+////    // Recreate emiiter layer.
+//    [self.emitterLayer removeFromSuperlayer];
+//    self.emitterLayer = nil;
+//    
+//    self.emitterLayer = [self emitterLayer];
+//    
+//    self.grayCircleImageCell.birthRate = floor(sender.value);
+//    
+//    [self.view.layer addSublayer: self.emitterLayer];
+//}
 
 
 #pragma mark - UIViewController
@@ -48,30 +46,69 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.emitterLayer = [self emitterLayer];
-    [self.view.layer addSublayer: self.emitterLayer];
-    self.birthRateSliderValueLabel.text = [NSString stringWithFormat:@"%0.f", self.birthRateSlider.value];
+    self.emitterLayer1 = [self emitterLayer1];
+    [self.view.layer addSublayer: self.emitterLayer1];
+    
+     self.emitterLayer2 = [self emitterLayer2];
+    [self.view.layer addSublayer: self.emitterLayer2];
+    
+    
+//    self.birthRateSliderValueLabel.text = [NSString stringWithFormat:@"%0.f", self.birthRateSlider.value];
 }
 
 
 #pragma mark - setup
--(CAEmitterLayer *)emitterLayer
+-(CAEmitterLayer *)emitterLayer1
+{
+    return [self emitterLayerAtPoint: CGPointMake(30, 100) WithCells: @[[self grayCircleImageCell]]];
+}
+
+
+-(CAEmitterLayer *)emitterLayer2
+{
+    return [self emitterLayerAtPoint: CGPointMake(30, 200) WithCells: @[[self firehoseGrayCircleImageCell]]];
+}
+
+-(CAEmitterLayer *)emitterLayerAtPoint: (CGPoint)point WithCells: (NSArray *)cells
 {
     CAEmitterLayer *emitterLayer = [CAEmitterLayer new];
-    emitterLayer.emitterPosition = CGPointMake(30, 100);
+    emitterLayer.emitterPosition = point;
     emitterLayer.emitterShape = kCAEmitterLayerPoint;
     emitterLayer.emitterMode   = kCAEmitterLayerPoints;
+
     
-    self.grayCircleImageCell = [self grayCircleImageCell];
-    
-    emitterLayer.emitterCells = @[[self grayCircleImageCell]];
+    emitterLayer.emitterCells = cells;
     return emitterLayer;
+}
+
+
+-(CAEmitterCell *)firehoseGrayCircleImageCell
+{
+    CAEmitterCell *cell = [CAEmitterCell emitterCell];
+    cell.birthRate = 100;
+    cell.lifetime = 1.6;
+    cell.velocity = 100;
+    cell.emissionRange = M_PI/5.0;
+    cell.contents = (id)[self grayCircle].CGImage;
+    
+    return cell;
 }
 
 
 -(CAEmitterCell *)grayCircleImageCell
 {
-    // Make a grey circele image.
+    CAEmitterCell *cell = [CAEmitterCell emitterCell];
+    cell.birthRate = 5;
+    cell.lifetime = 1;
+    cell.velocity = 100;
+    cell.contents = (id)[self grayCircle].CGImage;
+    
+    return cell;
+}
+
+-(UIImage *)grayCircle
+{
+    // Make a grey circle image.
     UIGraphicsBeginImageContextWithOptions((CGSizeMake(10, 10)), NO,1);
     CGContextRef con  = UIGraphicsGetCurrentContext();
     CGContextAddEllipseInRect(con, CGRectMake(0, 0, 10, 10));
@@ -79,16 +116,7 @@
     CGContextFillPath(con);
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    
-    // Make a cell with that image.
-    CAEmitterCell *cell = [CAEmitterCell emitterCell];
-    cell.birthRate = 5;
-    cell.lifetime = 1;
-    cell.velocity = 100;
-    cell.contents = (id)image.CGImage;
-    
-    return cell;
+    return image;
 }
-
 
 @end
