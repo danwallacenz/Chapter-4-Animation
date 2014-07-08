e05163567ca55595284b5a79ced1ecbdc733c14b
Daniel Wallace <daniel.wallace.nz@gmail.com>
2014-07-08 20:19:28 +1200

    add bounce

--- Chapter-4-Animations/Chapter-4-Animations/NinthViewController.m
+++ Chapter-4-Animations/Chapter-4-Animations/NinthViewController.m
@@ -8,7 +8,7 @@
 
 #import "NinthViewController.h"
 
-@interface NinthViewController ()
+@interface NinthViewController () <UICollisionBehaviorDelegate, UIDynamicAnimatorDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *mars;
 
@@ -20,6 +20,8 @@
 
 - (IBAction)runButtonPressed:(UIButton *)sender
 {
+    [sender setEnabled:NO];
+    // Fall stright down.
     UIGravityBehavior *gravity = [UIGravityBehavior new];
     /*
      If a dynamic behavior’s action block refers to the dynamic behavior
@@ -33,25 +35,77 @@
         if(NSNotFound ==[visibleItems indexOfObject:self.mars]){
             [self.animator removeAllBehaviors];
             [self.mars removeFromSuperview];
+            NSLog(@"%@", @"done");
         }
     };
     
     [self.animator addBehavior:gravity];
     [gravity addItem:self.mars];
     
+    // Git it a push to the right a bit.
     UIPushBehavior *push = [[UIPushBehavior alloc] initWithItems:@[self.mars] mode:UIPushBehaviorModeInstantaneous];
     push.pushDirection = CGVectorMake(2, 0);
+    [push setTargetOffsetFromCenter:UIOffsetMake(0, -200) forItem:self.mars];
     [self.animator addBehavior:push];
     
+    // Collide with the floor. Dead cat bounce.
     UICollisionBehavior *collision = [UICollisionBehavior new];
+    collision.collisionMode = UICollisionBehaviorModeBoundaries;
+    collision.collisionDelegate = self;
+    [collision addBoundaryWithIdentifier:@"floor"
+                               fromPoint:CGPointMake(0, self.view.bounds.size.height)
+                                 toPoint: CGPointMake(self.view.bounds.size.width, self.view.bounds.size.height)];
+    [self.animator addBehavior:collision];
+    [collision addItem:self.mars];
+    
+    
+    // Make it bounce up.
+    UIDynamicItemBehavior *bounce = [UIDynamicItemBehavior new];
+    bounce.elasticity = 0.4;
+    [self.animator addBehavior:bounce];
+    [bounce addItem:self.mars];
 }
 
+#pragma mark - UICollisionBehaviorDelegate
+
+-(void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p
+{
+    for (UIDynamicItemBehavior *theBehavior in self.animator.behaviors) {
+        if([theBehavior isKindOfClass:[UIDynamicItemBehavior class]]){
+            
+            NSLog(@"BoundaryIdentifier =  %@", identifier);
+            NSLog(@"%@", NSStringFromCGPoint(p));
+            
+            UIDynamicItemBehavior *bounce = (UIDynamicItemBehavior*)theBehavior;
+            CGFloat angularVelocity = [bounce angularVelocityForItem:self.mars];
+            NSLog(@"angularVelocity = %f", angularVelocity);
+            if(angularVelocity <= abs(0.1)){ // Do this just once.
+                 NSLog(@"%@", @"adding angular velocity");
+                [bounce addAngularVelocity:30 forItem:self.mars];
+                NSLog(@"angularVelocity now = %f", [bounce angularVelocityForItem:self.mars]);
+            }
+            break;
+        }
+    }
+}
+
+
+#pragma mark - UIViewCOntroller methods
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    self.animator.delegate = self;
 }
 
+#pragma mark - UIDynamicAnimatorDelegate methods
+-(void)dynamicAnimatorDidPause:(UIDynamicAnimator *)animator {
+    NSLog(@"%@", @"pause");
+}
+
+-(void)dynamicAnimatorWillResume:(UIDynamicAnimator *)animator {
+    NSLog(@"%@", @"resume");
+}
 @end
